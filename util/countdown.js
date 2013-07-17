2f4c581306c24f0005ae220d044c30b82ef49328
Masaki Hirokawa <info@dolice.net>
2013-07-17 20:21:02 +0900

    Fix countdown.js

--- util/countdown.js
+++ util/countdown.js
@@ -2,13 +2,18 @@
  * Countdown
  */
 var Countdown = {
-  hour : function(seconds) {
-    return (Math.floor((seconds / 60) / 60));
+  hour : function(remainingSeconds) {
+    return (Math.floor((remainingSeconds / 60) / 60));
   },
-  min : function(seconds) {
-    return (Math.floor(seconds / 60) % 60);
+  min : function(remainingSeconds) {
+    return (Math.floor(remainingSeconds / 60) % 60);
   },
-  sec : function(seconds) {
-    return (seconds % 60);
+  sec : function(remainingSeconds) {
+    return (remainingSeconds % 60);
+  },
+  doubleDigits : function(value) {
+    if (value < 10)
+      value = String('0' + value);
+    return (value);
   }
 }
